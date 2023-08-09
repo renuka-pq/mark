ff6c7b876c06fa17c6fcfc94cfde243169008735
renuka-pq <satyarenuka04@gmail.com>
2023-08-09 14:25:19 +0530

    done

---
+++ seleniumproject/src/conselenium/Bo.java
@@ -0,0 +1,16 @@
+package conselenium;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Bo {
+  @BeforeClass
+  
+
+@Test
+public void renu()
+{
+	}
+  
+
+}
---
+++ seleniumproject/src/conselenium/renu.java
@@ -0,0 +1,19 @@
+package conselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class renu {
+
+public class NewTest {
+  @Test
+  public void f() 
+  {
+	  WebDriver driver = new ChromeDriver ();
+	  driver.get("https://www.alpha-airtable.com/enterprise");
+  }
+}
+
+
+}
